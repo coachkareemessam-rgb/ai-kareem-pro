55602673c20c93c81f9f0a6c3e89c56509a8b32f
coachkareemessam-rgb <coachkareem.essam@gmail.com>
2026-02-10 19:31:39 +0300

    fix toaster import from shared

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route, Link } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@shared/components/ui/toaster";
 import { MainLayout } from "@/components/layout/MainLayout";
 import { RoleProvider, useRole } from "@/contexts/RoleContext";
 import RoleSelection from "@/pages/RoleSelection";
